2be08616dd1abec96d028c46457bc5e0b1529077
apatriz <patrizio@msu.edu>
2015-11-16 13:03:58 -0500

    Minor fixes

--- test_data/raw_data/csvparse.py
+++ test_data/raw_data/csvparse.py
@@ -11,8 +11,8 @@ json_out_location = "C:/Users/patrizio/Documents/GitHub/mapyou/test_data"
 for file in os.listdir(csvfile_location):
 	if not file.endswith('.csv'):
 		continue
-	csvRows = []
-	csvDict = []
+	csv_rows = []
+	obj_list = []
 	name = os.path.splitext(os.path.basename(file))[0]
 	json_out = os.path.join(json_out_location,'{0}.json'.format(name))
 	
@@ -25,19 +25,19 @@ for file in os.listdir(csvfile_location):
 	with open(file,'rb') as csv_file:
 		csv_reader = csvkit.py2.CSVKitReader(csv_file,encoding = codec)
 		for row in csv_reader:
-			csvRows.append(row)
-	headers = [header for header in csvRows[0]]
-	for row in csvRows:
-		if row == csvRows[0]:
+			csv_rows.append(row)
+	headers = [header for header in csv_rows[0]]
+	for row in csv_rows:
+		if row == csv_rows[0]:
 			continue
 		obj = OrderedDict()
 		for i in range(len(headers)):
 			obj[headers[i]] = row[i]
-		csvDict.append(obj) 
+		obj_list.append(obj) 
 	
 	with open(json_out,'wb') as json_file:
 		json_file.write('{0} = '.format(name))
-		json.dump(csvDict,json_file)
+		json.dump(obj_list,json_file)
 	
 
 	
